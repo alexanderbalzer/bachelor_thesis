9a29091859eefbb78f37954fa857570d338ba2f3
Alexander Balzer <alexander.m.balzer@gmail.com>
2025-03-21 15:00:44 +0100

    count instances at positions now working

--- Fasta_to_panda_DF.py
+++ Fasta_to_panda_DF.py
@@ -3,26 +3,28 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 import pandas as pd
+from Bio import SeqIO
 
 def fasta_to_dataframe(fasta_file):
     data = []
-    with open(fasta_file, 'r') as file:
-        for sequence in fp.Reader(file):
-            data.append([sequence.sequence])
-        return pd.DataFrame(data, columns=['sequence'])
+    for record in SeqIO.parse(fasta_file, "fasta"):
+        data.append([str(record.seq)])
+    return pd.DataFrame(data, columns=['sequence'])
+
 
 def count_instances_at_positions(array):
     rows, cols = array.shape
-    for col in range(min(cols, 20)):
+    dictlist = []
+    for col in range(cols):
         count_dict = {}
-        dictlist = []
         for row in range(rows):
             value = array[row, col]
             if (col, value) in count_dict:
-                count_dict[(col, value)] += 1
+                count_dict[(value)] += 1
             else:
-                count_dict[(col, value)] = 1
+                count_dict[(value)] = 1
         dictlist.append(count_dict)
+    print(dictlist)
     return dictlist
 
 
@@ -39,21 +41,15 @@ print(proteome_array)
 counted_instances = count_instances_at_positions(proteome_array)
 print(counted_instances)
 
-visual_array = np.zeros((len(amino_acid), len(position)))
 
-for count_dict in counted_instances:
-    for (col, value), count in count_dict.items():
-        row = np.where(amino_acid == value)[0][0]
-        visual_array[row, col] = count
+#fig, ax = plt.subplots()
+#im = ax.imshow(counted_instances, cmap="viridis")
+#ax.set_xticks(range(len(position)), labels=position, rotation=45, ha="right", rotation_mode="anchor")
+#ax.set_yticks(range(len(amino_acid)), labels=amino_acid)
 
-fig, ax = plt.subplots()
-im = ax.imshow(visual_array, cmap="viridis")
-ax.set_xticks(range(len(position)), labels=position, rotation=45, ha="right", rotation_mode="anchor")
-ax.set_yticks(range(len(amino_acid)), labels=amino_acid)
-
-for i in range(len(position)):
-    for j in range(len(amino_acid)):
-        text = ax.text(j, i, proteome[i, j], ha="center", va="center", color="w")
-ax.set_title("Mitochondrial Proteins with MTS")
-fig.tight_layout()
-plt.show()
+#for i in range(len(position)):
+#    for j in range(len(amino_acid)):
+#        text = ax.text(j, i, proteome[i, j], ha="center", va="center", color="w")
+#ax.set_title("Mitochondrial Proteins with MTS")
+#fig.tight_layout()
+#plt.show()
