Fix hypergeometric calculation and improve heatmap normalization for better visualization

Heatmap_of_first_AS_of_Proteins.py
@@ -31,10 +31,12 @@ def count_instances_at_positions(array):
 position = np.array(["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"])
 amino_acid = np.array(["D", "E", "N", "Q", "Y", "H", "K", "R", "M", "L", "F", "I", "W", "S", "A", "T", "C", "P", "G", "V"])
 
-input= ['output files/filtered_proteins_cleavable_mts.fasta', 'output files/filtered_proteins_no_cleavable_mts.fasta', 'output files/filtered_proteins.fasta']
+input= ['output files/filtered_proteins_cleavable_mts.fasta', 
+        'output files/filtered_proteins_no_cleavable_mts.fasta',
+        'output files/filtered_proteins.fasta']
 
 #wanted_result either "absolute" or "hgt"
-wanted_result = "absulute"
+wanted_result = "hgt"
 
 all_arrays = [0, 0, 0]
 all_counted_instances = [0, 0, 0]
@@ -81,7 +83,7 @@ if wanted_result == "hgt":
         cols = len(position)
         rows = len(amino_acid)
         visual_array = np.zeros((cols, rows))
-   #     print(visual_array)
+    #    print(visual_array)
         for i in range(rows):
             for j in range(cols):
                 if amino_acid[j] in counted_instances[i]:
@@ -89,32 +91,37 @@ if wanted_result == "hgt":
                     n = whole_set[i][amino_acid[j]] #amount of amino acid in the whole set
                     M = amount_of_proteins[2] #amount of all amino acids in the whole set
                     N = amount_of_proteins[file] #amount of all amino acids in the subset
-                    p_value = hypergeom.cdf(x, M, n, N)
+                    p_value = hypergeom.pmf(x, M, n, N)
                     abs_log_val = abs(np.log10(p_value))
                     f_obs = x / N
                     f_exp = n / M
-                    print(f_obs, f_exp)
+ #                   print(f_obs, f_exp)
+                    result = 0
                     if f_obs < f_exp:
-                        abs_log_val = -1 * abs_log_val
+                        result = -1 * abs_log_val
                     else:
-                        abs_log_val = abs_log_val
-                    visual_array[j, i] = abs_log_val
-
-        all_arrays[file]= visual_array
+                        result = abs_log_val
+                    visual_array[j, i] = result
  #       print(visual_array)
+        all_arrays[file]= visual_array
+        print(visual_array)
  #       print(visual_array.shape)
 
 
-
 fig, ax = plt.subplots(ncols= 2)
-pcm= ax[0].imshow(all_arrays[0], cmap="Blues")
-pcm = ax[1].imshow(all_arrays[1], cmap="Blues")
-
 
 ax[0].set_xticks(range(len(position)), labels=position, rotation=0, ha="right", rotation_mode="anchor")
 ax[0].set_yticks(range(len(amino_acid)), labels=amino_acid, rotation=0, rotation_mode="anchor")
 ax[0].set_yticklabels(amino_acid)
 ax[0].set_xticklabels(position)
+
+# Normalize the colormap across both graphs
+vmin = min(np.min(all_arrays[0]), np.min(all_arrays[1]))
+vmax = max(np.max(all_arrays[0]), np.max(all_arrays[1]))
+cmap = "coolwarm"
+pcm = ax[0].imshow(all_arrays[0], cmap=cmap, vmin=vmin, vmax=vmax)
+pcm = ax[1].imshow(all_arrays[1], cmap=cmap, vmin=vmin, vmax=vmax)
+
 ax[0].set_xlabel("Position")
 ax[0].set_ylabel("Amino acids")
 ax[0].set_title("Mitochondrial Proteins with MTS")
@@ -125,12 +132,11 @@ ax[1].set_xlabel("Position")
 ax[1].set_ylabel("Amino acids")
 ax[1].set_title("Mitochondrial Proteins without MTS")
 
-
-''''
+'''
 for i in range(len(position)):
     for j in range(len(amino_acid)):
-        text = ax.text(j, i, visual_array[i, j], ha="center", va="center", color="black")
+        text = ax.text(j, i, visual_array[i, j], ha="center", va="center", color="black")'
 '''
 fig.tight_layout()
-fig.colorbar(pcm, ax=ax, shrink=0.8)
+fig.colorbar(pcm, ax=ax, shrink=0.6)
 plt.show()